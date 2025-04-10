0eab48ae3a4c7006589ce6bf27fe92c88bda53ad
oaktree-ja <oaktree9056@gmail.com>
2025-04-10 19:10:44 +0900

    [add] 축제 리스트 css화면 구현 1차
    페이지네이션 해야함

--- src/main/java/com/fepeo/boot/festival/controller/FestivalController.java
+++ src/main/java/com/fepeo/boot/festival/controller/FestivalController.java
@@ -1,8 +1,19 @@
 package com.fepeo.boot.festival.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import lombok.RequiredArgsConstructor;
 
 @Controller
+@RequiredArgsConstructor
+@RequestMapping("/festival")
 public class FestivalController {
-
+	
+	@GetMapping("/list")
+	public String showFestivalList() {
+		return "festival/list";
+	}
+	
 }
