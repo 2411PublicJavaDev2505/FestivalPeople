d6ca9a3899df5768d8332568712874bddb003db0
jewoocoding <jewoocoding@gmail.com>
2025-04-11 12:41:36 +0900

    [ADD] 회원탈퇴 페이지 생성, MemberVO 생성, 회원가입페이지 성별 선택 추가
    1. ExceptionController 추가
    2. 회원탈퇴 페이지 생성
    3. Member value object 생성
    4. 회원가입 페이지 성별선택 추가 -> css 수정 필요
    
    TODO. 일반 회원가입, 일반 로그인, 파일올리기 구현 빨리하기

--- src/main/java/com/fepeo/boot/member/model/vo/Member.java
+++ src/main/java/com/fepeo/boot/member/model/vo/Member.java
@@ -1,5 +1,27 @@
 package com.fepeo.boot.member.model.vo;
 
+import lombok.Getter;
+import lombok.ToString;
+
+@Getter
+@ToString
 public class Member {
 
+	private int memberNo;
+	private String memberId;
+	private String memberPw;
+	private String memberCode;
+	private String memberName;
+	private String nickname;
+	private String email;
+	private String address;
+	private String gender;
+	private String profileFileName;
+	private String profileFileRename;
+	private String profileFilePath;
+	private String profileUrl;
+	private String socialYn;
+	private String memberYn;
+	private String managerYn;
+	private int reportCount;
 }
