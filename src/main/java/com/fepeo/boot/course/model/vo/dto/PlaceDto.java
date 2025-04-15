eb88367a0a07c70b5ba885f72148e6e5d9d3692e
DeveloperSoo <developsooket@gmail.com>
2025-04-15 09:04:01 +0900

    [ADD] 지도 API 연결
    1. APIComponent에 카카오map API 연결 확인

---
+++ src/main/java/com/fepeo/boot/course/model/vo/dto/PlaceDto.java
@@ -0,0 +1,28 @@
+package com.fepeo.boot.course.model.vo.dto;
+
+import lombok.Data;
+
+
+@Data
+public class PlaceDto {
+    private String place_name;
+    private String road_address_name;
+    private String phone;
+    private String x;
+    private String y;
+	public String getPlace_name() {
+		return place_name;
+	}
+	public String getRoad_address_name() {
+		return road_address_name;
+	}
+	public String getPhone() {
+		return phone;
+	}
+	public String getX() {
+		return x;
+	}
+	public String getY() {
+		return y;
+	}
+}
